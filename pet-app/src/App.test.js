ab4b5624a1f5680b8ad1713f3bae8e7bf6d260e7
Jillian Flook <jkenw10@gmail.com>
2018-12-14 15:50:32 -0600

    happy path testing for apollo element with enzyme

--- pet-app/src/App.test.js
+++ pet-app/src/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+
 import App from './App';
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App/>, div);
+  ReactDOM.render(
+  	<ApolloProvider client={{}}>
+  		<App/>
+	</ApolloProvider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
